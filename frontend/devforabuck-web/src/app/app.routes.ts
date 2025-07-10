1f3e2015b9a781f218c0e8940f0d419353d9c987
SabbirHasanAnik <sabbirhasananik@gmail.com>
2025-07-11 02:15:34 +0600

    create initial component and setup routing

--- frontend/devforabuck-web/src/app/app.routes.ts
+++ frontend/devforabuck-web/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+// import { HomeComponent } from '../app/pages/home/home';
+// import { AboutComponent } from './pages/about/about.component';
+// import { ContactComponent } from './pages/contact/contact.component';
+// import { BookingsComponent } from './pages/bookings/bookings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: '', component: HomeComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: 'contact', component: ContactComponent },
+  // { path: 'bookings', component: BookingsComponent }
+];
