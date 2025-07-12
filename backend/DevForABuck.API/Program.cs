d31df738c98e1403b5f872a607dc4ae2ca7bc413
SabbirHasanAnik <sabbirhasananik@gmail.com>
2025-07-12 18:10:46 +0600

    Tst booking apis

--- backend/DevForABuck.API/Program.cs
+++ backend/DevForABuck.API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // ✅ DI for custom services
-builder.Services.AddScoped<IBookingService, BookingService>();
+// builder.Services.AddScoped<IBookingService, BookingService>();
 
 // builder.Services.AddSingleton(s =>
 // {
