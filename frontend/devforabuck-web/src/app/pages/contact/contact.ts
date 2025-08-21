ef378c11421366a2ed28fa42323caf3d2eee6542
SabbirHasanAnik <sabbirhasananik@gmail.com>
2025-08-22 01:50:23 +0600

    Update design

--- frontend/devforabuck-web/src/app/pages/contact/contact.ts
+++ frontend/devforabuck-web/src/app/pages/contact/contact.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Footer } from '../../shared/footer/footer';
+import { MatDialog } from '@angular/material/dialog';
+import { BookingModalComponent } from '../components/modal/create-booking/create-booking';
 
 @Component({
   selector: 'app-contact',
@@ -9,5 +11,26 @@ import { Footer } from '../../shared/footer/footer';
   styleUrl: './contact.scss'
 })
 export class Contact {
+
+  constructor(private dialog: MatDialog) {}
+
+
+    openBookingModal() {
+      const dialogRef = this.dialog.open(BookingModalComponent, {
+        width: '600px',
+        maxWidth: '95vw',
+        panelClass: 'booking-dialog-container',
+        autoFocus: false,
+        backdropClass: 'booking-backdrop'
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          console.log('Booking confirmed:', result);
+        } else {
+          console.log('Booking cancelled');
+        }
+      });
+    }
 
 }
